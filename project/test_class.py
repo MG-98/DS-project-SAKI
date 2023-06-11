aa3c3ac452073f577dd64e021d35dceccb8f23b8
Mahmoud Sanad <mahmoud.gamal.sanad@fau.de>
2023-06-11 21:28:43 +0200

    updates

--- project/test_class.py
+++ project/test_class.py
@@ -13,19 +13,19 @@ class TestClass:
         """ Check the existence of the tables in the DB """
 
         try:
-            pd.read_sql_table('hotspots_in_koeln', '../data/project.sqlite')
+            pd.read_sql_table('hotspots_in_koeln', 'sqlite:///../data/project.sqlite')
         except :
             pytest.fail("'hotspots_in_koeln' table is not in the databse ..")    
         try:
-            pd.read_sql_table('road_sections', '../data/project.sqlite')
+            pd.read_sql_table('road_sections', 'sqlite:///../data/project.sqlite')
         except :
             pytest.fail("'road_sections' table is not in the databse ..")    
 
     def test_number_of_coloumsin_tables(self):
         """ Check the tables' number of coloums in the DB in the right number """
 
-        hotspots_in_koeln_df = pd.read_sql_table('hotspots_in_koeln', '../data/project.sqlite')
-        road_sections_df = pd.read_sql_table('road_sections', '../data/project.sqlite')
+        hotspots_in_koeln_df = pd.read_sql_table('hotspots_in_koeln', 'sqlite:///../data/project.sqlite')
+        road_sections_df = pd.read_sql_table('road_sections', 'sqlite:///../data/project.sqlite')
 
         assert len(hotspots_in_koeln_df.columns) == 8
         assert len(road_sections_df.columns) == 11
